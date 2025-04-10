04e380fca13e3b6574d189339bc88c34bfce2608
natimathias <130069163+natimathias@users.noreply.github.com>
2025-04-10 09:34:14 -0300

    Alteração
    Alteração na figura do index e teste na página principal

--- src/Principal.jsx
+++ src/Principal.jsx
@@ -1,5 +1,7 @@
 export default function Principal() {
     return (
-        <h1>Olá Mundo</h1>
+        <div className="h-3 w-5 bg-red-600">
+            <h1>Olá Mundo</h1>   
+        </div>
     )
 }
