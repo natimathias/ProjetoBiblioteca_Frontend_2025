a7d14ecabbde5b3e75c19297507a6cb9e246ed94
Driih Mendes <adrielymen@gmail.com>
2025-04-25 11:36:45 -0300

    exclusão do menu

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Rotas } from './Rotas'
-import { Menu } from './pages/Menu/Menu'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
