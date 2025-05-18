8fe3556b1569a147839de405391307b584931d17
Driih Mendes <adrielymen@gmail.com>
2025-05-18 01:20:49 -0300

    Criação_Simples
    Criação simples(sem estilização ainda) das paginas catalogo e meus dados

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Menu } from "./pages/Menu/Menu";
 import { Login } from "./pages/Login/Login";
 import { Pesquisar } from "./pages/Pesquisar/Pesquisar";
+import { Catalogo } from "./pages/catalogo/Catalogo";
+import { Dados } from "./pages/Dados/Dados";
 
 export function Rotas() {
 
@@ -11,6 +13,8 @@ export function Rotas() {
                 <Route path="/" element={<Menu />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/pesquisar" element={<Pesquisar />} />
+                {/* <Route path="/catalogo" element={<Catalogo />}/>
+                <Route path="/Dados" element={<Dados/>} /> */}
             </Routes>
         </BrowserRouter>
     )
